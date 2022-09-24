014490b3a7da6c79240ca7db1c42572595e0aef4
home <home@MSK-NB-POZ2.ad.kdl-test.ru>
2022-09-24 21:43:11 +0300

    Add test drafts

---
+++ src/test/java/ru/netology/carddelivery/CardDeliveryTest.java
@@ -0,0 +1,198 @@
+package ru.netology.carddelivery;
+
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Condition.enabled;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+
+    String dateShift(long daysForShift) {
+        Date currentDate = new Date();
+        Long time = currentDate.getTime();
+        time = time + (60 * 60 * 24 * 1000 * daysForShift);
+        currentDate = new Date(time);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+        return formatter.format(currentDate);
+    }
+
+    @BeforeEach
+    void siteOpen() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldGetSuccessMessageAfterCorrectFilling() {
+        Configuration.holdBrowserOpen = true;
+        $x("//input[@placeholder=\"Город\"]").setValue("Ростов-на-Дону");
+        $x("//input[@placeholder=\"Город\"]").sendKeys(Keys.TAB);
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick();
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.BACK_SPACE);
+        $x("//input[@placeholder=\"Дата встречи\"]").setValue(dateShift(4));
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.TAB);
+        $x("//*[@name='name']").setValue("Анна-Мария Иванова-Редгрейв");
+        $x("//*[@name='phone']").setValue("+89991234567");
+        $x("//*[@class=\"checkbox__box\"]").click();
+        $x("//div[@class=\"form-field form-field_size_m form-field_theme_alfa-on-white\"]/button[@role=\"button\"]").click();
+        $x("//div[contains(@class,'notification notification_visible')]/div[text()=\"Успешно!\"]").should(visible, Duration.ofSeconds(15));
+
+    }
+
+    @Test
+    void shouldGetWarningForIncorrectTown() {
+        Configuration.holdBrowserOpen = true;
+        $x("//input[@placeholder=\"Город\"]").setValue("Ростов-на-Дону-1");
+        $x("//input[@placeholder=\"Город\"]").sendKeys(Keys.TAB);
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick();
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.BACK_SPACE);
+        $x("//input[@placeholder=\"Дата встречи\"]").setValue(dateShift(4));
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.TAB);
+        $x("//*[@name='name']").setValue("Анна-Мария Иванова-Редгрейв");
+        $x("//*[@name='phone']").setValue("+89991234567");
+        $x("//*[@class=\"checkbox__box\"]").click();
+        $x("//div[@class=\"form-field form-field_size_m form-field_theme_alfa-on-white\"]/button[@role=\"button\"]").click();
+        $x("//span[@data-test-id=\"city\"]//span[@class=\"input__sub\"]").shouldHave(Condition.exactText("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void shouldGetWarningForIncorrectDate() {
+        Configuration.holdBrowserOpen = true;
+        $x("//input[@placeholder=\"Город\"]").setValue("Ростов-на-Дону");
+        $x("//input[@placeholder=\"Город\"]").sendKeys(Keys.TAB);
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick();
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.BACK_SPACE);
+        $x("//input[@placeholder=\"Дата встречи\"]").setValue(dateShift(-1));
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.TAB);
+        $x("//*[@name='name']").setValue("Анна-Мария Иванова-Редгрейв");
+        $x("//*[@name='phone']").setValue("+89991234567");
+        $x("//*[@class=\"checkbox__box\"]").click();
+        $x("//div[@class=\"form-field form-field_size_m form-field_theme_alfa-on-white\"]/button[@role=\"button\"]").click();
+        $x("//span[contains(@class,\"calendar-input\")]//span[@class=\"input__sub\"]").shouldHave(Condition.exactText("Заказ на выбранную дату невозможен"));
+    }
+
+    @Test
+    void shouldGetWarningForIncorrectName() {
+        Configuration.holdBrowserOpen = true;
+        $x("//input[@placeholder=\"Город\"]").setValue("Ростов-на-Дону");
+        $x("//input[@placeholder=\"Город\"]").sendKeys(Keys.TAB);
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick();
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.BACK_SPACE);
+        $x("//input[@placeholder=\"Дата встречи\"]").setValue(dateShift(4));
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.TAB);
+        $x("//*[@name='name']").setValue("Анна Иванова11");
+        $x("//*[@name='phone']").setValue("+89991234567");
+        $x("//*[@class=\"checkbox__box\"]").click();
+        $x("//div[@class=\"form-field form-field_size_m form-field_theme_alfa-on-white\"]/button[@role=\"button\"]").click();
+        $x("//span[@data-test-id=\"name\"]//span[@class=\"input__sub\"]").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldGetWarningForIncorrectPhone() {
+        Configuration.holdBrowserOpen = true;
+        $x("//input[@placeholder=\"Город\"]").setValue("Ростов-на-Дону");
+        $x("//input[@placeholder=\"Город\"]").sendKeys(Keys.TAB);
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick();
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.BACK_SPACE);
+        $x("//input[@placeholder=\"Дата встречи\"]").setValue(dateShift(4));
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.TAB);
+        $x("//*[@name='name']").setValue("Анна Иванова");
+        $x("//*[@name='phone']").setValue("+8999123456*");
+        $x("//*[@class=\"checkbox__box\"]").click();
+        $x("//div[@class=\"form-field form-field_size_m form-field_theme_alfa-on-white\"]/button[@role=\"button\"]").click();
+        $x("//span[@data-test-id=\"phone\"]//span[@class=\"input__sub\"]").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldGetRedTextIfCheckboxIsntSelect() {
+        Configuration.holdBrowserOpen = true;
+        $x("//input[@placeholder=\"Город\"]").setValue("Ростов-на-Дону");
+        $x("//input[@placeholder=\"Город\"]").sendKeys(Keys.TAB);
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick();
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.BACK_SPACE);
+        $x("//input[@placeholder=\"Дата встречи\"]").setValue(dateShift(4));
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.TAB);
+        $x("//*[@name='name']").setValue("Анна Иванова");
+        $x("//*[@name='phone']").setValue("+89991234567");
+        $x("//div[@class=\"form-field form-field_size_m form-field_theme_alfa-on-white\"]/button[@role=\"button\"]").click();
+        $x("//label[contains(@class, 'checkbox') and contains(@class, 'input_invalid')]").should(enabled);
+    }
+
+    @Test
+    void shouldGetWarningIfTownIsntFilling() {
+        Configuration.holdBrowserOpen = true;
+//        $x("//input[@placeholder=\"Город\"]").setValue("Ростов-на-Дону-1");
+//        $x("//input[@placeholder=\"Город\"]").sendKeys(Keys.TAB);
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick();
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.BACK_SPACE);
+        $x("//input[@placeholder=\"Дата встречи\"]").setValue(dateShift(4));
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.TAB);
+        $x("//*[@name='name']").setValue("Анна-Мария Иванова-Редгрейв");
+        $x("//*[@name='phone']").setValue("+89991234567");
+        $x("//*[@class=\"checkbox__box\"]").click();
+        $x("//div[@class=\"form-field form-field_size_m form-field_theme_alfa-on-white\"]/button[@role=\"button\"]").click();
+        $x("//span[@data-test-id=\"city\"]//span[@class=\"input__sub\"]").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldGetWarningIfDateIsntFilling() {
+        Configuration.holdBrowserOpen = true;
+        $x("//input[@placeholder=\"Город\"]").setValue("Ростов-на-Дону");
+        $x("//input[@placeholder=\"Город\"]").sendKeys(Keys.TAB);
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick();
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.BACK_SPACE);
+        //$x("//input[@placeholder=\"Дата встречи\"]").setValue(dateShift(-1));
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.TAB);
+        $x("//*[@name='name']").setValue("Анна-Мария Иванова-Редгрейв");
+        $x("//*[@name='phone']").setValue("+89991234567");
+        $x("//*[@class=\"checkbox__box\"]").click();
+        $x("//div[@class=\"form-field form-field_size_m form-field_theme_alfa-on-white\"]/button[@role=\"button\"]").click();
+        $x("//span[contains(@class,\"calendar-input\")]//span[@class=\"input__sub\"]").shouldHave(Condition.exactText("Неверно введена дата"));
+    }
+
+    @Test
+    void shouldGetWarningIfNameIsntFilling() {
+        Configuration.holdBrowserOpen = true;
+        $x("//input[@placeholder=\"Город\"]").setValue("Ростов-на-Дону");
+        $x("//input[@placeholder=\"Город\"]").sendKeys(Keys.TAB);
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick();
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.BACK_SPACE);
+        $x("//input[@placeholder=\"Дата встречи\"]").setValue(dateShift(4));
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.TAB);
+        //$x("//*[@name='name']").setValue("Анна Иванова11");
+        $x("//*[@name='phone']").setValue("+89991234567");
+        $x("//*[@class=\"checkbox__box\"]").click();
+        $x("//div[@class=\"form-field form-field_size_m form-field_theme_alfa-on-white\"]/button[@role=\"button\"]").click();
+        $x("//span[@data-test-id=\"name\"]//span[@class=\"input__sub\"]").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldGetWarningIfPhoneIsntFilling() {
+        Configuration.holdBrowserOpen = true;
+        $x("//input[@placeholder=\"Город\"]").setValue("Ростов-на-Дону");
+        $x("//input[@placeholder=\"Город\"]").sendKeys(Keys.TAB);
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick();
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.BACK_SPACE);
+        $x("//input[@placeholder=\"Дата встречи\"]").setValue(dateShift(4));
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.TAB);
+        $x("//*[@name='name']").setValue("Анна Иванова");
+        //$x("//*[@name='phone']").setValue("+8999123456*");
+        $x("//*[@class=\"checkbox__box\"]").click();
+        $x("//div[@class=\"form-field form-field_size_m form-field_theme_alfa-on-white\"]/button[@role=\"button\"]").click();
+        $x("//span[@data-test-id=\"phone\"]//span[@class=\"input__sub\"]").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+}
